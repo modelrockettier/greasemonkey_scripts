d981aae9afde1a1e86be40ee169d6c7cd0b3345c
Tim Schlueter <modelrockettier+github@gmail.com>
2013-01-08 15:54:33 -0800

    Improved lynn_classes
    Updated lynn_classes.js to add support for Google Chrome and its
    userscript functionality.  Also made the script more resilient with
    better checking to ensure we don't cause any errors, even if the site
    layout changes and elements we are relying on currently change.

--- lynn_classes.user.js
+++ lynn_classes.user.js
@@ -1,39 +1,49 @@
 // ==UserScript==
 // @name		Lynn Classes
-// @description	This script removes all known prevention measures in place that keep you from easily saving the content on Professor Douglas Lynn's website.
+// @description	This script removes all measures currently in place to keep you from easily accessing and saving various content on Professor Douglas Lynn's website.
 // @namespace	http://www.modelrockettier.com
-// @include	    http://www.cset.oit.edu/~lynnd/*
-// @version	    1
+// @match	    http://www.cset.oit.edu/~lynnd/*
+// @version	    1.1
 // ==/UserScript==
 
+// in greasemonkey we're only concerned with the actual window and document, not the wrappers
+if ( typeof(unsafeWindow) != 'undefined' )
+	window = unsafeWindow;
+if ( typeof(document.wrappedJSObject) != 'undefined' )
+	document = document.wrappedJSObject;
+
 // disable the onblur function (leaving the window, clicking on a different tab, etc.), which blanks the page content
-unsafeWindow.onblur = null;
+window.onblur = null;
 // disable the onfocus function (returning to/refocusing the window), which blanks the page content
-unsafeWindow.onfocus = null;
+window.onfocus = null;
 
 // disable the onmousedown function (when you click the mouse), which blanks page content
-document.wrappedJSObject.onmousedown = null;
+document.onmousedown = null;
 // disable the onkeydown function (when a key is pressed), which blanks the page content if ctrl is pressed
-document.wrappedJSObject.onkeydown = null;
+document.onkeydown = null;
 
-// clear the window timeout function
-unsafeWindow.clearTimeout(unsafeWindow.timerID);
+// clear the window timeout, if one exists
+if ( typeof(window.timerID) != 'undefined' )
+	window.clearTimeout( window.timerID );
 
 // remove any message about the window timing out
-if ( document.getElementsByTagName("span") && document.getElementsByTagName("span")[0].innerHTML.replace(/[ \t\n]+/g," ").substr(0,24) == "This window will timeout" )
-{
-	document.getElementsByTagName("span")[0].innerHTML = "This window will no longer timeout.  You're welcome.<br><br>\n";
+var span_tags = document.getElementsByTagName( "span" );
+if ( span_tags && span_tags[0] && span_tags[0].innerHTML ) {
+	if ( span_tags[0].innerHTML.replace(/[ \t\n]+/g," ").substr(0,24) == "This window will timeout" )
+		span_tags[0].innerHTML = "This window will no longer timeout.  You're welcome.<br><br>\n";
 }
 
 // restore the printing text style
-style = document.createElement('style');
-rules = document.createTextNode("body {display:inherit !important;}");
+var style = document.createElement( 'style' );
+var rules = document.createTextNode( 'body {display:inherit !important;}' );
 style.type = 'text/css';
 style.media = 'print';
 
-if(style.styleSheet)
+if ( style.styleSheet )
 	style.styleSheet.cssText = rules.nodeValue;
 else
-	style.appendChild(rules);
+	style.appendChild( rules );
 
-document.getElementsByTagName('head')[0].appendChild(style);
+var head = document.getElementsByTagName( 'head' );
+if ( head[0] )
+	head[0].appendChild( style );
